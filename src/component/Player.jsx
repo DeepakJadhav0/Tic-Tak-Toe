236a6c116d3c545f13cb098b0e27ff7d1946bce0
DeepakJadhav2003 <DeepakJadhav.drive@gmail.com>
2025-10-04 01:00:34 +0530

    Updated Player Component

--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react"
 
 export default function Player({name , symbol}) {
+    const [isediting , setisediting] = useState(false)
+    const [initialname , setinitialname] = useState(name)
+
+    function handelClickEdit(){
+        setisediting(isediting => !isediting)
+    }
+    let playerName =   <span className="player-name">{initialname}</span>
+
+    if(isediting){
+        playerName = <input value={initialname} onChange={handelPlayerName} type="text" required />
+    }
+
+    function handelPlayerName(event){
+        setinitialname(event.target.value)
+    }
   return (
         <li>
           <span className="player">
-              <span className="player-name">{name}</span>
-              <span className="player-symbol">{symbol}</span>
+            {playerName}
+            <span className="player-symbol">{symbol}</span>
           </span>
-          <button>Edit</button>
+          <button onClick={handelClickEdit}>{isediting ? "Save" : "Edit"}</button>
         </li>
   )
 }
